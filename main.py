a83a164a7c4498041feacd91a94a5acac5f58fdf
Yasser <yasserfedsi34@gmail.com>
2025-08-07 18:02:16 +0200

    Update .gitignore, modify index.html, and restructure vercel.json; add main.py for Flask app initialization and routing

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from core.routes.requests import requests_bp
+from core.routes.templates import templates_bp
+from core.routes.certificates import certificates_bp
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(requests_bp)
+app.register_blueprint(certificates_bp)
+app.register_blueprint(templates_bp)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
