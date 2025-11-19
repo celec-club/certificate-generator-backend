a3df924166cd3cf3061de2077fb2ae2e5557d3d9
Yasser <yasserfedsi34@gmail.com>
2025-11-19 12:32:07 +0100

    Remove index.html template and update index route to return JSON response

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from core.routes.certificates import certificates_bp
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(requests_bp)
+app.register_blueprint(requests_bp, )
 app.register_blueprint(certificates_bp)
 app.register_blueprint(templates_bp)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return jsonify({"service": "Certificate Generator Backend", "status": "online"})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
