f35008a112f8d426d905eb871383517606c288ce
Yasser <yasserfedsi34@gmail.com>
2025-11-23 21:20:53 +0100

    Refactor CORS initialization and remove unused frontend route

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from flask_cors import CORS
 from core.api.certificates.routes import CertificateRoutes
 from core.api.requests.routes import CertificateRequestRoutes
 from core.api.templates.routes import TemplateRoutes
-from frontend.routes import frontend
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
-if not app.secret_key:
-    raise ValueError("No SESSION_SECRET set for Flask application")
 
 # -------------------------------
 # Initialize CORS
 # -------------------------------
-CORS(app, origins=["*"])
+CORS(
+    app,
+    origins=["*"],
+)
 
 # -------------------------------
 # Register blueprints
@@ -27,7 +25,6 @@ template_routes = TemplateRoutes()
 app.register_blueprint(certificates_routes.bp)
 app.register_blueprint(certificate_requests_routes.bp)
 app.register_blueprint(template_routes.bp)
-app.register_blueprint(frontend)
 
 
 # -------------------------------
