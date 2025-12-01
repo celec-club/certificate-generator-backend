4584b088724e82f23c966b72efa2cb2bc54dcbe6
Yasser <yasserfedsi34@gmail.com>
2025-12-01 21:55:43 +0100

    Restrict CORS origins to localhost for enhanced security

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 # -------------------------------
 CORS(
     app,
-    origins=["*"],
+    origins=["http://localhost:5173"],
+    supports_credentials=True,
 )
 
 # -------------------------------
